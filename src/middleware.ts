ca36c75d51bf62595a3c852d1bd77329679578b1
Macbook Pro 2020 <macbookpro2020@192.168.1.11>
2025-10-31 19:24:55 +0000

    fix: add uploadthing token and simplify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Routes publiques
-const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/api/uploadthing(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth.protect();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
